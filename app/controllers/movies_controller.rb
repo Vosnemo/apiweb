77bad64d9b09e254fe9aedcdbe3b5dcf4d719f57
Guille <nemocastillo@hotmail.com>
2021-08-30 20:01:14 -0600

    Creacion del controller Movie

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,48 @@
+class MoviesController < ApplicationController
+    
+    #GET /movies
+    def index
+        @movies = Movie.all
+        render 200, json: @movies
+    end
+
+
+    def show
+        @movie = Movie.find(params[:id])
+        render 200, json: @movie
+    end
+
+    #POST /movies
+    def create
+        @movie = Movie.new(movie_params)
+
+        if @movie.save
+            render 201, json: @movie 
+        else
+            render 401, json: @movie.errors
+        end
+    end
+
+    #PUT /movies/:id
+    def update
+        @movie = Movie.find(params[:id])
+        
+        if @movie.update(movie_params)
+            render 200, json: @movie
+        else
+            render 422, json: @movie.errors
+        end
+    end
+
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        head 204
+        
+    end
+
+    protected 
+    def movie_params
+        params.require(:movie).permit(:name, :synopsis, :genre, :classification, :score, :duration, :year)
+    end
+end
